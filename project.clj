e709d915d2fc42acabab566fba3811e7082f634d
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-01-15 10:42:41 -0500

    updated project structure to be NPM compatible

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject macchiato/hiccups "0.4.1"
-  :description "A ClojureScript port of Hiccup - a fast library for rendering HTML in ClojureScript"
+(defproject macchiato/hiccups "0.4.2"
+  :description "a library for rendering HTML in ClojureScript using Hiccup syntax"
   :url "https://github.com/macchiato-framework/hiccups"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.2.0"
   :clojurescript? true
-  :source-paths ["src/clj" "src/cljs"]
+  :source-paths ["src"]
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.562"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.597"]]
 
   :plugins [[lein-doo "0.1.7"]
             [lein-cljsbuild "1.1.4"]]
@@ -18,7 +18,7 @@
              {:cljsbuild
                    {:builds
                     {:test
-                     {:source-paths ["src/clj" "src/cljs" "test/cljs"]
+                     {:source-paths ["src" "test/cljs"]
                       :compiler     {:main          hiccups.runner
                                      :output-to     "target/test/core.js"
                                      :target        :nodejs
